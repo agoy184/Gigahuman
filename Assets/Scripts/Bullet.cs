2a0844d342885a718beda97740a0b94a6d1efc74
Gigz <gigzpumpking@gmail.com>
2023-11-29 21:27:18 -0800

    Combat mechanics added, stun/slow gun

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -14,4 +14,17 @@ public class Bullet : MonoBehaviour
         yield return new WaitForSeconds(2f);
         gameObject.SetActive(false);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<EnemyStatus>().TakeDamage(10);
+            if (other.gameObject.GetComponent<EnemyStatus>().hp > 0) {
+                other.gameObject.GetComponent<EnemyNavMesh>().Stun(1f);
+                other.gameObject.GetComponent<EnemyNavMesh>().Slow(2f, 0.2f);
+            }
+            gameObject.SetActive(false);
+        }
+    }
 }
